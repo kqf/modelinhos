c3d6f753f289c4972e16794debd4ea909000251f
kqf <q.dev.null@gmail.com>
2025-11-27 08:53:49 +0100

    Reverse the dependency (#13)
    * Create postprocessing
    
    * Fix the jaccard
    
    * Remove the overlap similarity

--- modelinhos/blaze/blazenet.py
+++ modelinhos/blaze/blazenet.py
@@ -511,8 +511,3 @@ def intersect(box_a, box_b):
     )
     inter = torch.clamp((max_xy - min_xy), min=0)
     return inter[:, :, 0] * inter[:, :, 1]
-
-
-def overlap_similarity(box, other_boxes):
-    """Computes the IOU between a bounding box and set of other boxes."""
-    return jaccard(box.unsqueeze(0), other_boxes).squeeze(0)
