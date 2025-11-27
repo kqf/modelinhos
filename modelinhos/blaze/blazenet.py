a508cff6eacc39c11087441c9c3df954e20d5854
kqf <q.dev.null@gmail.com>
2025-11-27 08:46:08 +0100

    Add the postprocessing (#12)
    * Create postprocessing
    
    * Fix the jaccard

--- modelinhos/blaze/blazenet.py
+++ modelinhos/blaze/blazenet.py
@@ -1,3 +1,4 @@
+from modelinhos.blaze.postprocessing import jaccard
 import numpy as np
 import torch
 import torch.nn as nn
@@ -510,33 +511,6 @@ def intersect(box_a, box_b):
     )
     inter = torch.clamp((max_xy - min_xy), min=0)
     return inter[:, :, 0] * inter[:, :, 1]
-
-
-def jaccard(box_a, box_b):
-    """Compute the jaccard overlap of two sets of boxes.  The jaccard overlap
-    is simply the intersection over union of two boxes.  Here we operate on
-    ground truth boxes and default boxes.
-    E.g.:
-        A ∩ B / A ∪ B = A ∩ B / (area(A) + area(B) - A ∩ B)
-    Args:
-        box_a: (tensor) Ground truth bounding boxes, Shape: [num_objects,4]
-        box_b: (tensor) Prior boxes from priorbox layers, Shape: [num_priors,4]
-    Return:
-        jaccard overlap: (tensor) Shape: [box_a.size(0), box_b.size(0)]
-    """
-    inter = intersect(box_a, box_b)
-    area_a = (
-        ((box_a[:, 2] - box_a[:, 0]) * (box_a[:, 3] - box_a[:, 1]))
-        .unsqueeze(1)
-        .expand_as(inter)
-    )  # [A,B]
-    area_b = (
-        ((box_b[:, 2] - box_b[:, 0]) * (box_b[:, 3] - box_b[:, 1]))
-        .unsqueeze(0)
-        .expand_as(inter)
-    )  # [A,B]
-    union = area_a + area_b - inter
-    return inter / union  # [A,B]
 
 
 def overlap_similarity(box, other_boxes):
