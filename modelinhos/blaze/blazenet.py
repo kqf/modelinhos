01e185173cd73f4aed6e9e96c1646d722f6c91f7
kqf <q.dev.null@gmail.com>
2025-11-29 06:17:52 +0100

    Move decoding (#16)
    * Create postprocessing
    
    * Fix the jaccard
    
    * Remove the overlap similarity
    
    * Extract even more methods outside
    
    * Move the NMS
    
    * Move the decoding

--- modelinhos/blaze/blazenet.py
+++ modelinhos/blaze/blazenet.py
@@ -396,34 +396,3 @@ def _tensors_to_detections(self, raw_box_tensor, raw_score_tensor, anchors):
         output_detections.append(torch.cat((boxes, scores), dim=-1))
 
     return output_detections
-
-
-def _decode_boxes(self, raw, anchors):
-    """Converts the predictions into actual coordinates using
-    the anchor boxes. Processes the entire batch at once.
-    """
-    boxes = torch.zeros_like(raw)
-
-    x_center = raw[..., 0] / self.x_scale * anchors[:, 2] + anchors[:, 0]
-    y_center = raw[..., 1] / self.y_scale * anchors[:, 3] + anchors[:, 1]
-
-    w = raw[..., 2] / self.w_scale * anchors[:, 2]
-    h = raw[..., 3] / self.h_scale * anchors[:, 3]
-
-    boxes[..., 0] = y_center - h / 2.0  # ymin
-    boxes[..., 1] = x_center - w / 2.0  # xmin
-    boxes[..., 2] = y_center + h / 2.0  # ymax
-    boxes[..., 3] = x_center + w / 2.0  # xmax
-
-    for k in range(6):
-        offset = 4 + k * 2
-        keypoint_x = (
-            raw[..., offset] / self.x_scale * anchors[:, 2] + anchors[:, 0]
-        )
-        keypoint_y = (
-            raw[..., offset + 1] / self.y_scale * anchors[:, 3] + anchors[:, 1]
-        )
-        boxes[..., offset] = keypoint_x
-        boxes[..., offset + 1] = keypoint_y
-
-    return boxes
