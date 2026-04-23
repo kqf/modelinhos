a59e2f7f54a9c412c74d8c691498cf6f4587b7dd
kqf <q.dev.null@gmail.com>
2026-04-23 07:23:05 +0100

    Add the custom anchor parametrization

--- modelinhos/ssd/retianent_tv.py
+++ modelinhos/ssd/retianent_tv.py
@@ -1,9 +1,42 @@
+from itertools import product
+from math import ceil
+
+import torch
 from torchvision.models.detection.retinanet import (
     LastLevelP6P7,
 )
 
-from modelinhos.ssd.anchors import anchors
 from modelinhos.ssd.retinanet import RetinaNetPure
+
+
+def retinanet_anchors(image_size, steps, min_sizes, aspect_ratios):
+    anchors = []
+
+    feature_maps = [
+        [ceil(image_size[0] / s), ceil(image_size[1] / s)] for s in steps
+    ]
+
+    for k, (fm_h, fm_w) in enumerate(feature_maps):
+        for i, j in product(range(fm_h), range(fm_w)):
+            cx = (j + 0.5) * steps[k] / image_size[1]
+            cy = (i + 0.5) * steps[k] / image_size[0]
+
+            for size in min_sizes[k]:
+                area = size * size
+                for ar in aspect_ratios:
+                    w = (area / ar) ** 0.5
+                    h = w * ar
+
+                    anchors.append(
+                        [
+                            cx,
+                            cy,
+                            w / image_size[1],
+                            h / image_size[0],
+                        ]
+                    )
+
+    return torch.tensor(anchors).view(-1, 4)
 
 
 def build_retinanet_torchvision(resolution: tuple[int, int]):
@@ -12,13 +45,13 @@ def build_retinanet_torchvision(resolution: tuple[int, int]):
         extra_blocks=LastLevelP6P7(2048, 256),
         num_anchors=9,
     )
-    priors = anchors(
-        [
-            [16, 20],  # P3  (stride 8)
-            [32, 40],  # P4  (stride 16)
-            [64, 80],  # P5  (stride 32)
-            [128, 160],  # P6  (stride 64)
-            [256, 320],  # P7  (stride 128)
+    priors = retinanet_anchors(
+        min_sizes=[
+            [32, 40, 50],  # P3
+            [64, 80, 100],  # P4
+            [128, 160, 200],
+            [256, 320, 400],
+            [512, 640, 800],
         ],
         steps=[
             8,
@@ -28,7 +61,7 @@ def build_retinanet_torchvision(resolution: tuple[int, int]):
             128,
         ],
         image_size=resolution,
-        clip=False,
+        aspect_ratios=[0.5, 1.0, 2.0],
     )
 
     return model, priors
