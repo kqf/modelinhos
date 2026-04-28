c6da73761d4c76328e10e898972b091d29b1cf64
kqf <q.dev.null@gmail.com>
2026-04-28 07:29:33 +0100

    10 add new anchors (#57)
    * Fix the model's inference
    
    * Unskip the tests
    
    * Add the retinanet tv
    
    * Add the retinanet
    
    * Check the weights definitions are identical
    
    * Fix the retina-net
    
    * Add the custom anchor parametrization
    
    * Fix the inference
    
    * Make the scores similar
    
    * Fix the anchors
    
    * Make sure anchros are the same
    
    * Fix the tests
    
    * Move the retinanet TV tests to a proper location
    
    * Fix the resolution
    
    * Add the standard resolution
    
    * Fix the tests
    
    * Enable all the tests / boost coverage
    
    * Add the generalized anchors

--- modelinhos/ssd/anchors.py
+++ modelinhos/ssd/anchors.py
@@ -1,5 +1,5 @@
 from itertools import product
-from math import ceil
+from math import ceil, sqrt
 
 import torch
 
@@ -31,4 +31,36 @@ def anchors(
     output = torch.Tensor(anchors).view(-1, 4)
     if clip:
         output.clamp_(max=1, min=0)
+    return output
+
+
+def anchors2(
+    sizes: list[list[int]],
+    steps: list[int],
+    image_size: tuple[int, int],
+    aspect_ratios: list[float],
+    scales: list[float],
+    clip=False,
+):
+    H, W = image_size
+    feature_maps = [[ceil(H / step), ceil(W / step)] for step in steps]
+    out = []
+    for k, f in enumerate(feature_maps):
+        for i, j in product(range(f[0]), range(f[1])):
+            cx = (j + 0.5) * steps[k] / W
+            cy = (i + 0.5) * steps[k] / H
+
+            for base_size in sizes[k]:
+                for ar in aspect_ratios:
+                    for scale in scales:
+                        s = base_size * scale
+                        w = round((s / sqrt(ar)) / 2) * 2 / W
+                        h = round((s * sqrt(ar)) / 2) * 2 / H
+                        out += [cx, cy, w, h]
+
+    output = torch.tensor(out).view(-1, 4)
+
+    if clip:
+        output.clamp_(0, 1)
+
     return output
---
+++ tests/ssd/test_anchors.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from modelinhos.ssd.anchors import anchors, anchors2
+
+
+def test_anchors(resolution):
+    priors = anchors(
+        image_size=resolution,
+        min_sizes=[[16, 32], [64, 128], [256, 512]],
+        steps=[8, 16, 32],
+        clip=False,
+    )
+
+    priors2 = anchors2(
+        sizes=[[16, 32], [64, 128], [256, 512]],
+        steps=[8, 16, 32],
+        image_size=resolution,
+        aspect_ratios=[1.0],
+        scales=[1.0],
+        clip=False,
+    )
+
+    np.testing.assert_almost_equal(
+        priors.cpu().numpy(),
+        priors2.cpu().numpy(),
+        decimal=4,
+    )
