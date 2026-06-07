67181fbe4c66a1eff11c07821227db9d251ba3be
kqf <q.dev.null@gmail.com>
2026-06-07 09:13:39 +0200

    Fix/label position (#123)
    * Introduce the flag
    
    * Fix the annotations
    
    * Accept paths only
    
    * Refactor samples
    
    * Fix the coco dataset
    
    * Plot the labels
    
    * Simplify the plotter interface
    
    * Fix the typing
    
    * Fix
    
    * Convert predictions
    
    * Fix again
    
    * Fix the inference
    
    * Fix the label position
    
    * Fix the weights again
    
    * Parametrize label location
    
    * Fix eerything

--- tests/ssd/test_inference.py
+++ tests/ssd/test_inference.py
@@ -107,7 +107,7 @@ def frame(resolution, path: str = "tests/assets/person.jpg") -> np.ndarray:
         partial(
             Detector,
             # Don't multiply by two because it breaks tests.
-            build_model=partial(bulid_retinanet, n_classes=92 * 1),
+            build_model=partial(bulid_retinanet, n_classes=91 * 1),
             th=0.01,
             weights=RetinaNet_ResNet50_FPN_V2_Weights.COCO_V1,
         ),
