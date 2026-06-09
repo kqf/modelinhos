5e4f08cb6037ddce4c72c568b5037d99f076b4af
kqf <q.dev.null@gmail.com>
2026-06-09 08:24:11 +0200

    Add the label encoder (#125)
    * Remove the loops outside
    
    * Fix the tests
    
    * Fix the inference tests
    
    * Add the label encoder protocol
    
    * AdImplement do nothing encoder
    
    * Fix the torchvision encoder

--- modelinhos/ssd/inference.py
+++ modelinhos/ssd/inference.py
@@ -1,4 +1,5 @@
 import math
+from typing import Protocol, runtime_checkable
 
 import cv2
 import numpy as np
@@ -84,6 +85,26 @@ def postprocess(preds, priors, resolution, score_thresh=0.4, iou_thresh=0.5):
     return Sample(file_name=None, annotations=list(annotations[0]))
 
 
+@runtime_checkable
+class LabelEncoderType(Protocol):
+    def fit_transform(self, samples: list[Sample]) -> list[Sample]: ...
+
+    def transform(self, samples: list[Sample]) -> list[Sample]: ...
+
+    def inverse_transform(self, samples: list[Sample]) -> list[Sample]: ...
+
+
+class DoNothingEncoder:
+    def fit_transform(self, samples: list[Sample]) -> list[Sample]:
+        return samples
+
+    def transform(self, samples: list[Sample]) -> list[Sample]:
+        return samples
+
+    def inverse_transform(self, samples: list[Sample]) -> list[Sample]:
+        return samples
+
+
 class Detector:
     def __init__(
         self,
@@ -93,6 +114,7 @@ class Detector:
         th=0.4,
         postprocess=postprocess,
         normalize=normalize,
+        lencoder: LabelEncoderType = DoNothingEncoder(),
     ):
         self.model, self.priors = build_model(
             resolution=resolution,
@@ -104,12 +126,19 @@ class Detector:
         self.normalize = normalize
         self.th = th
         self.resolution = resolution
+        self.label_encoder = lencoder
+
+    def fit(self, samples: list[Sample]) -> "Detector":
+        self.label_encoder.fit_transform(samples)
+        return self
 
     def transform(self, samples: list[Sample]) -> list[Sample]:
-        return [
-            self.transform_single(cv2.imread(str(s.file_name)))
-            for s in tqdm.tqdm(samples)
-        ]
+        return self.label_encoder.inverse_transform(
+            [
+                self.transform_single(cv2.imread(str(s.file_name)))
+                for s in tqdm.tqdm(samples)
+            ]
+        )
 
     def transform_single(self, frame: np.ndarray) -> Sample:
         self.model.eval()
@@ -150,6 +179,7 @@ class TorchvisionDetector(Detector):
         th=0.4,
         postprocess=torchvison_to_samples,
         normalize=lambda x: x,
+        lencoder: LabelEncoderType = DoNothingEncoder(),
     ):
         self.model = build_model(
             resolution=resolution,
@@ -162,3 +192,4 @@ class TorchvisionDetector(Detector):
         self.normalize = normalize
         self.th = th
         self.resolution = resolution
+        self.label_encoder = lencoder
