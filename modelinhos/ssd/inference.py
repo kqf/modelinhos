7e939f8d6037fe1a83a7bfc642523e59bf9c09e0
kqf <q.dev.null@gmail.com>
2026-07-05 07:14:46 +0200

    Abstract away the training loop

--- modelinhos/ssd/inference.py
+++ modelinhos/ssd/inference.py
@@ -1,9 +1,8 @@
 from functools import partial
-from typing import Callable, Protocol, runtime_checkable
+from typing import Callable, Optional, Protocol, runtime_checkable
 
 import numpy as np
 import torch
-import tqdm
 
 from modelinhos.postprocess import (
     Collate,
@@ -16,23 +15,6 @@ from modelinhos.postprocess import (
 from modelinhos.preprocess.image import build_transform, normalize
 from modelinhos.preprocess.lables import DoNothingEncoder
 from modelinhos.sample import Sample
-
-DataloaderBuilder = Callable[
-    [torch.utils.data.Dataset, Callable],
-    torch.utils.data.DataLoader,
-]
-
-
-def default_dataloader_builder(
-    dataset,
-    collate_fn,
-) -> torch.utils.data.DataLoader:
-    return torch.utils.data.DataLoader(
-        dataset,
-        batch_size=1,
-        num_workers=0,
-        collate_fn=collate_fn,
-    )
 
 
 @runtime_checkable
@@ -50,58 +32,45 @@ class SampleEncoder(Protocol):
 class Detector:
     def __init__(
         self,
-        build_model=lambda: print,
-        build_metrics=None,
+        build_model: Callable,
+        trainer: Callable,
         lencoder: SampleEncoder = None,
-        train_dataloader: DataloaderBuilder = default_dataloader_builder,
-        valid_dataloader: DataloaderBuilder = default_dataloader_builder,
     ):
         self.model, self.transforms, self.decode, self.w, collate = (
             build_model()
         )
         self.label_encoder = lencoder or DoNothingEncoder()
-        self.train_dataloader = train_dataloader
-        self.valid_dataloader = valid_dataloader
-        self.loss = print
-        self.metrics = build_metrics
         self.collate = collate
+        self._trainer = trainer(
+            self.model,
+            self.decode,
+            self.collate,
+            self.label_encoder,
+        )
 
-    def fit(self, samples: list[Sample]) -> "Detector":
+    def fit(
+        self, samples: list[Sample], val_samples: Optional[list[Sample]] = None
+    ) -> "Detector":
         encoded = self.label_encoder.transform(samples)
         dataset = SampleDataset(encoded, self.transforms)
-        loader = self.train_dataloader(dataset, self.collate.collate)
-        self.model.train()
-        for images, batch in tqdm.tqdm(loader):
-            preds = self.model(images)
-            losses = self.loss(batch, preds)
-            print(losses)
-            true = self.label_encoder.inverse_transform(
-                self.collate.un_batch(batch),
-            )
-            pred = self.label_encoder.inverse_transform(
-                self.collate.un_batch_nms(self.decode(preds))
-            )
-            self.metrics(true, pred)
+
+        val_dataset = None
+        if val_samples is not None:
+            val_encoded = self.label_encoder.transform(val_samples)
+            val_dataset = SampleDataset(val_encoded, self.transforms)
+
+        self._trainer.fit(dataset, val_dataset)
         return self
 
     def transform(self, samples: list[Sample]) -> list[Sample]:
         encoded = self.label_encoder.transform(samples)
         dataset = SampleDataset(encoded, self.transforms)
-        loader = self.valid_dataloader(dataset, self.collate.collate)
-        self.model.eval()
-        results = []
-        with torch.no_grad():
-            for images, _ in tqdm.tqdm(loader):
-                results.extend(
-                    self.collate.un_batch_nms(self.decode(self.model(images)))
-                )
-        return self.label_encoder.inverse_transform(results)
+        preds = self._trainer.predict(dataset)
+        return self.label_encoder.inverse_transform(preds)
 
     def transform_single(self, frame: np.ndarray) -> list[Sample]:
-        self.model.eval()
         blob = self.transforms(frame).unsqueeze(0)
-        with torch.no_grad():
-            preds = self.collate.un_batch_nms(self.decode(self.model(blob)))
+        preds = self._trainer.predict_single(blob)
         return self.label_encoder.inverse_transform(preds)
 
 
