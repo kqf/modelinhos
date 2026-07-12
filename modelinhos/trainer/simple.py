b5e8fcdce175f4fbfbf9821fc02b416f0edbbd0f
kqf <q.dev.null@gmail.com>
2026-07-12 06:23:00 +0200

    17 add evaluation to trainer (#184)
    * Move processing pipeline
    
    * Rename the file
    
    * Simplify the inference pipeline
    
    * Move box decoding to own module
    
    * Remove the filenames from the pipeline
    
    * Re-arrange the funcitons ordering
    
    * Modularize decode
    
    * Fix the inference
    
    * Make things work
    
    * Fix the labelbug
    
    * Fix the nms
    
    * Rename the fields
    
    * Fix the bboxes
    
    * Refactor the entire pipeline
    
    * Make genric samples
    
    * Fix the type errors
    
    * Make things work
    
    * Rename the samples
    
    * Fix the todo
    
    * Abstract away the training loop
    
    * Fix the inference
    
    * Add the sample encoder
    
    * Fix the inference
    
    * Fix the metrics
    
    * Add the metric collector
    
    * Add the trainer evaluation

--- modelinhos/trainer/simple.py
+++ modelinhos/trainer/simple.py
@@ -3,6 +3,8 @@ from typing import Callable, Optional
 import torch
 import tqdm
 
+from modelinhos.evaluation import MetricCollector
+
 DLBuilder = Callable[
     [torch.utils.data.Dataset, Callable],
     torch.utils.data.DataLoader,
@@ -48,7 +50,7 @@ class SimpleTrainer:
         self.collate = collate
         self.lencoder = label_encoder
         self.loss_fn = loss_fn
-        self.metrics_fn = metrics
+        self.metrics_fn = metrics or MetricCollector
         self.train_dataloader_builder = train_dataloader_builder
         self.valid_dataloader_builder = valid_dataloader_builder
         self.epochs = epochs
@@ -59,23 +61,22 @@ class SimpleTrainer:
         self.model = model.to(self.device)
         self.optimizer = optimizer_builder(self.model.parameters(), lr)
 
-    def _score(self, batch, preds):
-        if self.metrics_fn is None:
-            return
-
+    def _score(self, metric_fn, batch, preds):
         true = self.lencoder.inverse_transform(
             self.collate.un_batch(batch),
         )
         pred = self.lencoder.inverse_transform(
             self.collate.un_batch_nms(self.decode(preds))
         )
-        self.metrics_fn(true, pred)
+        metric_fn(true, pred)
 
     def fit(self, dataset, val_dataset=None) -> "SimpleTrainer":
         loader = self.train_dataloader_builder(dataset, self.collate.collate)
 
-        for _ in range(self.epochs):
+        for epoch in range(self.epochs):
             self.model.train()
+            trainm = self.metrics_fn(self.lencoder.l2i)
+            validm = self.metrics_fn(self.lencoder.l2i)
             for images, targets in tqdm.tqdm(loader):
                 images = images.to(self.device)
                 preds = self.model(images)
@@ -86,14 +87,17 @@ class SimpleTrainer:
                     loss.backward()
                     self.optimizer.step()
 
-                self._score(targets, preds)
+                self._score(trainm, targets, preds)
 
             if val_dataset is not None:
-                self._validate(val_dataset)
+                self._validate(validm, val_dataset)
+
+            print(f"Epoch {epoch}, train mAP: {trainm.value().iloc[0]['mAP']}")
+            print(f"Epoch {epoch}, train mAP: {validm.value().iloc[0]['mAP']}")
 
         return self
 
-    def _validate(self, dataset) -> None:
+    def _validate(self, metrics_fn, dataset) -> None:
         loader = self.valid_dataloader_builder(dataset, self.collate.collate)
         self.model.eval()
         with torch.no_grad():
@@ -101,7 +105,7 @@ class SimpleTrainer:
                 images = images.to(self.device)
                 preds = self.model(images)
                 self.loss_fn(targets, preds)
-                self._score(targets, preds)
+                self._score(metrics_fn, targets, preds)
 
     def predict(self, dataset) -> list:
         loader = self.valid_dataloader_builder(dataset, self.collate.collate)
