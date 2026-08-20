b99aa751723e3906db50bdef1aa3a4fd6c525feb
kqf <q.dev.null@gmail.com>
2026-08-20 07:03:59 +0200

    Feature/narrow down the conftest (#263)
    * Make sure the models are being trained
    
    * Fix the comments
    
    * Unify the fixtures
    
    * Narrow down the conftest

--- tests/models/conftest.py
+++ tests/models/conftest.py
@@ -4,11 +4,5 @@ import pytest
 
 
 @pytest.fixture
-def resolution() -> tuple[int, int]:
-    # This is magic resolution to avoid additional geometric conversion
-    return 800, 1088
-
-
-@pytest.fixture
 def headless():
     return os.environ.get("DISPLAY", "") == ""
